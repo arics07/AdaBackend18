5b6860ea96c77241e1504dea50fd6efff63c8c24
Ariadna Salomone <48843076+arics07@users.noreply.github.com>
2024-09-14 16:07:52 -0300

    Add files via upload

--- clase32/ejercicio4/ejercicio4.js
+++ clase32/ejercicio4/ejercicio4.js
@@ -8,3 +8,23 @@ y muestra la lista ordenada.
 
 const readlineSync = require('readline-sync');
 
+// Pido al usuario que ingrese una lista de números separados por comas
+let lista = readlineSync.question('Ingresa una lista de números separados por comas: ');
+
+// Convertir la cadena de entrada en un array de números
+let numeros = lista.split(',').map(num => Number(num));
+//La parte .map(num => Number(num)) convierte cada elemento de la lista (que originalmente es un string) en un número.
+
+// Ordeno los números de menor a mayor usando el método .sort
+let numerosOrdenados = numeros.sort((a, b) => a - b);
+//(a, b) => a - b: Esta es una función de comparación que se pasa como argumento a sort(). 
+//La función recibe dos elementos del array (a y b) y devuelve un número que indica su orden:
+//Si a - b es negativo: a se coloca antes que b en el array.
+//Si a - b es positivo: b se coloca antes que a.
+//Si a - b es cero: a y b se mantienen en el mismo orden relativo.
+
+console.log('Lista de números ordenada:', numerosOrdenados.join(', '));
+
+
+
+
--- clase32/moduloCrypto/metodosCrypto.js
+++ clase32/moduloCrypto/metodosCrypto.js
@@ -1,5 +1,9 @@
 const crypto = require('crypto');
 
+
+//-------------------------------------------------
+//-------- crypto.createHash ----------------------
+//-------------------------------------------------
 const data = "Este es un mensaje secreto";
 //Se crea un objeto hash utilizando el método createHash
 const hash = crypto.createHash('sha256');
@@ -11,6 +15,10 @@ const hashOutput = hash.digest('hex');
 console.log("Hash SHA-256: ", hashOutput);
 
 console.log("-----------------------------------------------------");
+
+//-------------------------------------------------
+//-------- crypto.createHmac ----------------------
+//-------------------------------------------------
 
 const data2 = "Mensaje secreto, shhhh";
 const secretKey = "mi_clave_secreta";
@@ -25,4 +33,40 @@ console.log("HMAC SHA-256: ", hmacOutput);
 
 console.log("-----------------------------------------------------");
 
-//1.52
+//-------------------------------------------------
+//-------- crypto.createCypheriv----------------------
+//-------------------------------------------------
+
+// Generar un vector de inicialización (IV) aleatorio
+//El vector de inicialización (IV) es un valor que se utiliza en algoritmos de cifrado como el AES en modo CBC (Cipher Block Chaining). 
+//El IV garantiza que dos mensajes cifrados con la misma clave pero con diferentes IV generen diferentes resultados cifrados.
+const iv = crypto.randomBytes(16);
+
+//La clave key es el valor utilizado por el algoritmo para cifrar y descifrar los datos. 
+//estamos generando una clave aleatoria de 32 bytes (256 bits), ya que estamos utilizando el algoritmo AES-256, que requiere una clave de ese tamaño
+const key = crypto.randomBytes(32);
+const algoritmo = 'aes-256-cbc';
+
+// Crear un objeto Cipher utilizando AES-256-CBC
+const cipher = crypto.createCipheriv(algoritmo, key, iv);
+
+let data3 = "Soy el texto a cifrar";
+// Cifrar el texto
+let encrypted = cipher.update(data3, 'utf-8', 'hex');
+encrypted += cipher.final('hex');
+
+// Devolver el IV junto con el texto cifrado
+//console.log("Texto cifrado: " + iv.toString('hex') + ':' + encrypted);
+// Devuelvo el texto cifrado
+console.log("Texto cifrado: " + encrypted);
+
+// DESENCRIPTADO
+
+ // Crear un objeto Decipher utilizando AES-256-CBC
+ const decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+
+ // Descifrar el texto
+ let decrypted = decipher.update(encrypted, 'hex', 'utf-8');
+ decrypted += decipher.final('utf-8');
+
+console.log("Texto descifrado: " + decrypted);
